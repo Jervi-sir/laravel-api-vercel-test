d2c36536c1d6ffb05db717b20dfb5920992d1c67
Jervi Sir <gacembekhira@gmail.com>
2024-05-10 19:03:06 +0100

    .

--- api/index.php
+++ api/index.php
@@ -1,6 +1,5 @@
 <?php  
 
-require __DIR__ . "/../public/index.php";
 // Load the Laravel application
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
