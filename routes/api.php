f274113a8415ed4d511baec181ce0bf1868c855d
Jervi-sir <gacembekhira@gmailcom>
2024-05-10 01:55:07 +0100

    .

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test', function (Request $request) {
+    return response()->json([
+         'message' => 'api working'
+    ]);
+});
